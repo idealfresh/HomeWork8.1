03980288652d7df8d1b4623e2a4baae827869694
owner90 <owner90@192.168.5.9>
2016-10-19 10:06:13 +0300

    Метод greetings

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void f1(String name) {
+		// Напишете метод, който при подадено име отпечатва в конзолата "Greetings, <name>!" (Напишете програма, която тества този метод.)
+System.out.println("Greetings " + name+ ". The master of stupidity");
+	}
+	public static void main(String[] args){
+		f1("Nasko");
+	}
+
+}
